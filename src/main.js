3c7a5a57485fa86d9c0bed431e57e5ffc62f0c72
Farhan Wundari <squidwardsus@Squidwardsus.local>
2025-04-28 22:23:23 +0700

    update: main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import { loggerApp } from './application/logger.js';
+// import { loggerApp } from './application/logger.js';
+// import { web } from './application/web.js';
+
+
+// web.listen(3000, () => {
+//   loggerApp.info('Server started on port 3000');
+// });
+
+
 import { web } from './application/web.js';
 
-
-web.listen(3000, () => {
-  loggerApp.info('Server started on port 3000');
-});
+// Jangan listen, cukup export default
+export default web;
