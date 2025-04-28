6ae6ff90a30e97f965bfc2759d5d1e6b6808b1ec
Farhan Wundari <squidwardsus@Squidwardsus.local>
2025-04-28 23:17:25 +0700

    add: logger

--- src/application/logger.js
+++ src/application/logger.js
@@ -1,1 +1,11 @@
-da
+import winston from "winston";
+
+
+export const loggerApp = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({})
+  ]
+});
+
