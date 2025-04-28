cb3173d6f46ab3a910ba7007ff616fcf31bcfd07
Farhan Wundari <squidwardsus@Squidwardsus.local>
2025-04-28 22:36:42 +0700

    fix: structur

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-// import { loggerApp } from './application/logger.js';
-// import { web } from './application/web.js';
-
-
-// web.listen(3000, () => {
-//   loggerApp.info('Server started on port 3000');
-// });
-
-
+import { loggerApp } from './application/logger.js';
 import { web } from './application/web.js';
 
-// Jangan listen, cukup export default
-export default web;
+
+web.listen(3000, () => {
+  loggerApp.info('Server started on port 3000');
+});
